f2908a7959291615807768d370abf2c1279a3392
oukichikou <oukichikou@DESKTOP-0OV4DTG>
2019-08-28 15:15:45 +0800

    1

--- shiro_test/src/test/java/shiro_test/test1.java
+++ shiro_test/src/test/java/shiro_test/test1.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import controller.testController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
